e872470d651af69d9f54eb6becd3a16f895c6611
ilhomadilovdev <ilkhamadilov1999@gmail.com>
2024-10-02 15:53:56 +0500

    yuklash

---
+++ js.js
@@ -0,0 +1,8 @@
+
+window.onload = function() {
+   
+    setTimeout(function() {
+      const myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+      myModal.show();
+    }, 1000);
+  };
